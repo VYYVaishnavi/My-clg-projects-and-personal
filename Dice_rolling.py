5d51c4f90386e2bfa6ce39bbbdf074292249fea2
VYYVaishnavi <solankivaishnavi2008@gmail.com>
2026-05-04 19:48:54 +0530

    Update Dice_rolling.py

--- Dice_rolling.py
+++ Dice_rolling.py
@@ -232,47 +232,6 @@ if __name__=='__main__':
 #         else:
 #            print("please enter the valid number!!")
   
-#ROCK PAPER AND SCISSOR GAME-----
-# import random
-# print("---------ROCK PAPER AND SCISSOR GAME--------")
-# won=0
-# lose=0
-# Tie=0
-# while True:
-#  emojis={'r':'🪨','p':'📄','s': '✂️'}
-#  choices=('r','p','s')
-#  user_choice=input("ROCK🪨 PAPER📑 SCISSORS (r/p/s): ").lower()
-#  if user_choice not in choices:
-#    print("please choose any one from above choices")
-#    continue
-#  computer_guess=random.choice(choices)
-#  print(f"you chose {emojis[user_choice]}")
-#  print(f"computer chose {emojis[computer_guess]}")
-#  won+=1
-#  if (
-#    user_choice=='r'and computer_guess=='s' or
-#    user_choice=='p'and computer_guess=='r' or
-#    user_choice=='s'and computer_guess):
-#    print("YOU WON..")
-#    Tie+=1
-#  elif user_choice==computer_guess:
-#    print("Tie..!,Try Again")
-#    lose+=1
-#  else:
-#    print("SORRY.. YOU LOSE ")
-   
-#  should_continue=input("Continue..? (y/n):")
-#  if should_continue=='y':
-#    print("")
-#  else:
-#    print("THANK YOU FOR PLAYING")
-#    print(f"You Won=={won}")
-#    print(f"You Lose=={lose}")
-#    print(f"Total Tie=={Tie} ")
-#    break
-
-
-   
 
 
 
